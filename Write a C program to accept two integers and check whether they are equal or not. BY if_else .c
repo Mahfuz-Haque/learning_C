80bc52b543ff176fc5c63ec9923c6e8c48f6ddb5
Mahfuz-Haque <109871178+Mahfuz-Haque@users.noreply.github.com>
2022-07-24 14:26:42 +0600

    Add files via upload

---
+++ Write a C program to accept two integers and check whether they are equal or not. BY if_else .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("Enter the fast number a: Enter the second number b:\n");
+scanf("%d%d",&a,&b);
+if(a==b)
+    printf("The number is Equal");
+
+  else
+    printf("The number is not equal");
+}
