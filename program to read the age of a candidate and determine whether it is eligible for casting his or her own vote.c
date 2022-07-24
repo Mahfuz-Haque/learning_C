682117ffad1a7f68d39cd45bc54a569dbeaed101
Mahfuz-Haque <109871178+Mahfuz-Haque@users.noreply.github.com>
2022-07-25 01:05:18 +0600

    Update program to read the age of a candidate and determine whether it is eligible for casting his or her own vote.c

--- program to read the age of a candidate and determine whether it is eligible for casting his or her own vote.c
+++ program to read the age of a candidate and determine whether it is eligible for casting his or her own vote.c
@@ -4,11 +4,9 @@ int main()
     int x;
     printf("Enter your ege: ");
     scanf("%d",&x);
-    if (x<18)
-        printf("your age is:%d you can not give vote",x);
+    if (x>=18)
+        printf("your age is:%d you can give vote",x);
     else
-        printf("You can give vote");
+        printf("You can not give vote");
     return 0;
-
-
 }
