9a09ddd846ff973ef492cc17b64ba4bf781f7c81
Mahfuz-Haque <109871178+Mahfuz-Haque@users.noreply.github.com>
2022-07-25 23:58:23 +0600

    Update program to find the largest of three numbers.c

--- program to find the largest of three numbers.c
+++ program to find the largest of three numbers.c
@@ -15,3 +15,25 @@ int main()
         return 0;
 
 }
+
+
+right
+
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter three numbers:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b || b>a)
+        {
+        printf("the fast big number is :%d\n");
+        if(a>c || c>a)
+       {
+           printf("the second big number is:%d");
+       }
+
+       }
+        return 0;
+
+}
