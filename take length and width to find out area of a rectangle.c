fbff7f5251c73d70c3dbc9c79897b40c1275c745
Mahfuz-Haque <109871178+Mahfuz-Haque@users.noreply.github.com>
2022-11-16 19:15:32 +0600

    Add files via upload

---
+++ take length and width to find out area of a rectangle.c
@@ -0,0 +1,12 @@
+//take length and width to find out area of a rectangle
+#include<stdio.h>
+int main()
+{
+    int wight ,length ,area;
+    printf("Ehter the wight and length :");
+    scanf("%d%d",&wight,&length);
+    area=wight*length;
+    printf("The area is :%d",area);
+    return 0;
+
+}
