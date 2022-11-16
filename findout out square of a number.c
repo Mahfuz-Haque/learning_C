5f99199502eed0620094dfbc71057d3ee244a84a
Mahfuz-Haque <109871178+Mahfuz-Haque@users.noreply.github.com>
2022-11-16 19:19:16 +0600

    Add files via upload

---
+++ findout out square of a number.c
@@ -0,0 +1,12 @@
+//findout out square of a number
+#include<stdio.h>
+int main()
+{
+    int n,square;
+    printf("Ehter the number :");
+    scanf("%d",&n);
+    square=n*2;
+    printf("The square is :%d",square);
+    return 0;
+
+}
