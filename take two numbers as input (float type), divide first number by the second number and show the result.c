3605763469baf4891532dc57056b5b0d1a1b70fa
Mahfuz-Haque <109871178+Mahfuz-Haque@users.noreply.github.com>
2022-11-16 18:52:24 +0600

    Add files via upload

---
+++ take two numbers as input (float type), divide first number by the second number and show the result.c
@@ -0,0 +1,12 @@
+//take two numbers as input (float type), divide first number by the second number and show the result
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    printf("Enter two numbers:");
+    scanf("%f%f",&a,&b);
+    c=a/b;
+    printf("The divide number is :%d",c);
+    return 0;
+
+}
