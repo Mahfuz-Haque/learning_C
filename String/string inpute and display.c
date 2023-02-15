f6070e65470c755415272cc8f93be5928d958b8a
Mahfuz Haque <109871178+Mahfuz-Haque@users.noreply.github.com>
2023-02-16 00:46:58 +0600

    Create string inpute and display.c

---
+++ String/string inpute and display.c
@@ -0,0 +1,12 @@
+// string inpute and display
+#include<stdio.h>
+int main()
+{
+    char s[100];
+
+    printf ("Enter your name here: ");
+    gets (s);
+
+    printf("Hello %s",s);
+    return 0;
+}
